54fa5247c543d3f2d50c0dfeb3c003dfaa663eae
Qingchao Meng <noeveah@gmail.com>
2020-09-14 19:03:30 -0700

    Python Flask Web Server

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'b6309ec031df5aadc4e2aade82f04291'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
--- run.py
+++ run.py
@@ -1,5 +1,4 @@
 from flaskblog import app
 
-
-if  __name__ == '__main__':
+if __name__ == '__main__':
     app.run(debug=True)
